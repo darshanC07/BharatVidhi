3763c18a3c67401cc60e6468abd28e89a6a3d007
Darshan <darshanchoudhary2007@gmail.com>
2025-02-16 16:25:53 +0530

    completed touchable part and popping up effect

--- components/Footer.js
+++ components/Footer.js
@@ -1,28 +1,82 @@
-import { Text, View, Image } from 'react-native'
+import React, { useState } from 'react'
+import { Text, View, Image, StyleSheet, TouchableHighlight, TouchableWithoutFeedback } from 'react-native'
 
 export default function Footer() {
+    let defaultState = {
+        icon1:false,
+        icon2:false,
+        icon3:false,
+        icon4:false,
+        icon5:false
+    }
+    const [tabBar,setTabBar] = useState(defaultState)
 
+
+    function handlePress(i){
+        let temp = defaultState
+        let icon = "icon"+i
+        temp[icon] = true
+        setTabBar(temp)
+        // console.log(tabBar)
+    }
     return (
         <View style={{
             // flex: 1,
             flexDirection: 'row',
             justifyContent: 'space-between',
-            height: 70,
-            backgroundColor: '#FFDCB9',
+            height: 65,
+            backgroundColor: 'lightblue',
             position: 'absolute',
             bottom: 0,
             // top:0,
-            left:0,
-            right:0,
-            padding:10
+            left: 0,
+            right: 0,
+            paddingLeft: 10,
+            paddingRight: 10,
         }}>
-            <Image source={require('../assets/icon1.png')}/>
-            <Image source={require('../assets/icon2.png')}/>
-            <Image source={require('../assets/icon3.png')}/>
-            <Image source={require('../assets/icon4.png')}/>
-            <Image source={require('../assets/icon5.png')}/>
+            <TouchableWithoutFeedback onPress={()=>{handlePress(1)}} >
+                <View style={tabBar.icon1? styles.onClickStyle:styles.highLightEffect}>
+                    <Image source={require('../assets/icon1.png')} />
+                </View>
+            </TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={()=>{handlePress(2)}} >
+            <View style={tabBar.icon2? styles.onClickStyle:styles.highLightEffect}>
+                <Image source={require('../assets/icon2.png')} />
+            </View></TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={()=>{handlePress(3)}} >
+            <View style={tabBar.icon3? styles.onClickStyle:styles.highLightEffect}>
+                <Image source={require('../assets/icon3.png')} />
+            </View></TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={()=>{handlePress(4)}} >
+            <View style={tabBar.icon4? styles.onClickStyle:styles.highLightEffect}>
+                <Image source={require('../assets/icon4.png')} />
+            </View></TouchableWithoutFeedback>
+            <TouchableWithoutFeedback onPress={()=>{handlePress(5)}} >
+            <View style={tabBar.icon5? styles.onClickStyle:styles.highLightEffect}>
+                <Image source={require('../assets/icon5.png')} />
+            </View></TouchableWithoutFeedback>
+
 
         </View>
     )
 
 }
+
+let styles = StyleSheet.create({
+    highLightEffect: {
+        height: 70,
+        backgroundColor: 'lightblue',
+        paddingTop: 5,
+        width: 70,
+        alignContent: 'center',
+        alignItems: 'center',
+    },
+    onClickStyle: {
+        bottom: 15,
+        borderBottomLeftRadius: '50%',
+        borderBottomRightRadius: '50%',
+        borderTopLeftRadius: '50%',
+        borderTopRightRadius: '50%',
+        backgroundColor: 'lightblue',
+    }
+})

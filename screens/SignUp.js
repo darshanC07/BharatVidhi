56fc2b0959d69968f6c48491b06530907dc05999
Darshan <darshanchoudhary2007@gmail.com>
2025-02-18 19:53:05 +0530

    fixed issue of height of content

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,9 +1,8 @@
-import { StatusBar } from 'expo-status-bar';
+// import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View, Image, TextInput, KeyboardAvoidingView, ScrollView, TouchableWithoutFeedback ,Platform} from 'react-native';
+import { StyleSheet, Text, View, Image, TextInput, KeyboardAvoidingView, ScrollView, TouchableWithoutFeedback, Platform, StatusBar , Dimensions} from 'react-native';
 
 export default function App() {
-
   const [email, setEmail] = useState('')
   const [name, setName] = useState('')
   const [password, setPassword] = useState('')
@@ -14,14 +13,14 @@ export default function App() {
     console.log('name = ', name)
     console.log('email = ', email)
     console.log('password = ', password)
-    let response = await fetch("https://9tj0pwqw-5000.inc1.devtunnels.ms/signup",{
-      method:"POST",
+    let response = await fetch("https://9tj0pwqw-5000.inc1.devtunnels.ms/signup", {
+      method: "POST",
       headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ 
-        firstname : name,
-        email : email,
-        password : password
-       })
+      body: JSON.stringify({
+        firstname: name,
+        email: email,
+        password: password
+      })
     })
     let data = await response.json()
     setUserResp(data)
@@ -30,54 +29,56 @@ export default function App() {
   }
 
   return (
-    <ScrollView style={{ flex: 1 }}>
+    <ScrollView style={styles.outerContainer}>
       <KeyboardAvoidingView>
-      <View style={styles.container}>
+        <View style={styles.container}>
 
-        <Image source={require('../assets/signup/Rectangle_1.png')} style={styles.flag1}></Image>
-        <Image source={require('../assets/signup/Rectangle_2.png')} style={styles.flag2}></Image>
-        <Image source={require('../assets/signup/chakra2.png')} style={styles.bg1}></Image>
-        <Image source={require('../assets/signup/chakra1.png')} style={styles.bg2}></Image>
-        <Image source={require('../assets/signup/chakra3.png')} style={styles.bg3}></Image>
-        <View style={styles.borderContainer}>
-          <Text style={styles.text}>BHARAT VIDHI</Text>
-          <Text style={styles.signUp}>SIGN UP</Text>;
-          <View style={styles.rectangleView}>
-            <TextInput style={styles.inputField} placeholder='Email-id' value={email} onChangeText={(text) => setEmail(text)} />
-          </View>
-          <View style={styles.rectangleView}>
-            <TextInput style={styles.inputField} placeholder='Name' value={name} onChangeText={(text) => setName(text)} /></View>
-          <View style={styles.rectangleView}>
-            <TextInput style={styles.inputField} placeholder='Password' value={password} onChangeText={(text) => setPassword(text)} /></View>
-          <View style={styles.rectangleView}>
-            <TextInput style={styles.inputField} placeholder='Confirm Password' value={confirmPassword} onChangeText={(text) => setComfirmPassword(text)} /></View>
-          <View style={styles.rectangleIcon} />;
-          <TouchableWithoutFeedback onPress={handleProceed}><View style={styles.proceedBox}><Text style={styles.proceedText}>{userResp}</Text></View></TouchableWithoutFeedback>
-          <View style={styles.google}><Text style={styles.googleText}>Sign Up using Google
-            {/* <Image source={require('./assets/Search.png')} style={styles.googleImage}></Image> */}
-          </Text>
-          </View>
-          <View style={{
-            alignItems:'center',
-            justifyContent:'center'
-          }}>
-            <Text style={styles.toLogin}>Already have a account?</Text>
-            <Text style={styles.logIn}>Log in!</Text>
+          <Image source={require('../assets/signup/Rectangle_1.png')} style={styles.flag1}></Image>
+          <Image source={require('../assets/signup/Rectangle_2.png')} style={styles.flag2}></Image>
+          <Image source={require('../assets/signup/chakra2.png')} style={styles.bg1}></Image>
+          <Image source={require('../assets/signup/chakra1.png')} style={styles.bg2}></Image>
+          <Image source={require('../assets/signup/chakra3.png')} style={styles.bg3}></Image>
+          <View style={styles.borderContainer}>
+            <Text style={styles.text}>BHARAT VIDHI</Text>
+            <Text style={styles.signUp}>SIGN UP</Text>;
+            <View style={styles.rectangleView}>
+              <TextInput style={styles.inputField} placeholder='Email-id' value={email} onChangeText={(text) => setEmail(text)} />
+            </View>
+            <View style={styles.rectangleView}>
+              <TextInput style={styles.inputField} placeholder='Name' value={name} onChangeText={(text) => setName(text)} /></View>
+            <View style={styles.rectangleView}>
+              <TextInput style={styles.inputField} placeholder='Password' value={password} onChangeText={(text) => setPassword(text)} /></View>
+            <View style={styles.rectangleView}>
+              <TextInput style={styles.inputField} placeholder='Confirm Password' value={confirmPassword} onChangeText={(text) => setComfirmPassword(text)} /></View>
+            <View style={styles.rectangleIcon} />;
+            <TouchableWithoutFeedback onPress={handleProceed}><View style={styles.proceedBox}><Text style={styles.proceedText}>{userResp}</Text></View></TouchableWithoutFeedback>
+            <View style={styles.google}><Text style={styles.googleText}>Sign Up using Google
+              {/* <Image source={require('./assets/Search.png')} style={styles.googleImage}></Image> */}
+            </Text>
+            </View>
+            <View style={{
+              alignItems: 'center',
+              justifyContent: 'center'
+            }}>
+              <Text style={styles.toLogin}>Already have a account?
+              <Text style={styles.logIn}>Log in!</Text></Text>
 
 
+            </View>
           </View>
-        </View>
-      </View></KeyboardAvoidingView>
-     </ScrollView >
+        </View></KeyboardAvoidingView>
+    </ScrollView >
   );
 }
 
 const styles = StyleSheet.create({
-  scrollContainer: {
+  outerContainer: {
     flex: 1,
-    marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+    marginTop:Platform.OS == "android" ? StatusBar.currentHeight:0
+  
   },
   container: {
+    height: Dimensions.get('window').height,
     flex: 1,
     justifyContent: 'top',
     alignItems: 'center',
@@ -90,6 +91,7 @@ const styles = StyleSheet.create({
     borderColor: '#FFDCB9',
     borderRadius: 10,
     padding: 20,
+    paddingTop:0,
     marginTop: "10%",
     marginBottom: "5%",
     justifyContent: 'center',

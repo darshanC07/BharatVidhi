0605fa2b5f19c2fee18be0f3a4acb73d95c17562
Darshan <darshanchoudhary2007@gmail.com>
2025-02-19 16:51:43 +0530

    my branch state before pulling another branch state

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 // import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, SafeAreaView, View, Alert, Image, ScrollView, Button, Platform, StatusBar } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Homepage from './components/Homepage';
+import SignUp from './screens/SignUp';
+
 
 export default function App() {
-  
+
+  const Stack = createNativeStackNavigator();
+
   // console.log(StatusBar.currentHeight)
   return (
-    <Homepage></Homepage>
-    // <Login></Login>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="SignUp">
+        <Stack.Screen name="Homepage" component={Homepage} options={{headerShown : false}}/>
+        <Stack.Screen name="SignUp" component={SignUp} options={{headerShown : false}}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 

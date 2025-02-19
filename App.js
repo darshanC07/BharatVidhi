f1e2cc4c00b2501c30144ed02ac36595dd7877a8
Darshan <darshanchoudhary2007@gmail.com>
2025-02-19 18:59:23 +0530

    state before merging

--- App.js
+++ App.js
@@ -3,40 +3,26 @@ import { StyleSheet, Text, SafeAreaView, View, Alert, Image, ScrollView, Button,
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Homepage from './components/Homepage';
-// import Login from './components/Login_temp';
 import Login from './screens/Login';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-const Stack = createStackNavigator();
 import SignUp from './screens/SignUp';
-
+// import Login from './Login'
 
 export default function App() {
 
   const Stack = createNativeStackNavigator();
 
+  
+
   // console.log(StatusBar.currentHeight)
   return (
     // <Homepage></Homepage>
     // <Login></Login>
+    
     <NavigationContainer>
-    <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen 
-        name="Login" 
-        component={Login} 
-        options={{ headerShown: false }} 
-      />
-      <Stack.Screen 
-        name="Homepage" 
-        component={Homepage} 
-        options={{ headerShown: false }} 
-      />
-    </Stack.Navigator>
-  </NavigationContainer>
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="SignUp">
+      <Stack.Navigator initialRouteName="Login">
         <Stack.Screen name="Homepage" component={Homepage} options={{headerShown : false}}/>
         <Stack.Screen name="SignUp" component={SignUp} options={{headerShown : false}}/>
+        <Stack.Screen name="Login" component={Login} options={{headerShown : false}}/>
       </Stack.Navigator>
     </NavigationContainer>
   );

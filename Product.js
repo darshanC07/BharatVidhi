4e9ab3eb4239f3591d9af4b12b3c3cfc5a344263
Prabodh1007 <162123156+Prabodh1007@users.noreply.github.com>
2025-03-01 13:30:24 +0530

    Add files via upload

---
+++ Product.js
@@ -0,0 +1,146 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View , TouchableOpacity,SafeAreaView,Image, Platform, PixelRatio,ScrollView} from 'react-native';
+import Footer from './Footer';
+import { useFonts, PatrickHandSC_400Regular } from '@expo-google-fonts/patrick-hand-sc';
+import { KiwiMaru_400Regular } from '@expo-google-fonts/kiwi-maru';
+
+const ProductDisplay = ({name, description, price, image, onPress, accquired, total }) => {
+  const [fontsLoaded] = useFonts({
+            KiwiMaru_400Regular,
+            PatrickHandSC_400Regular
+            });
+    return (
+      <View style={{
+        backgroundColor: 'rgba(255, 238, 221, 0.4)',
+        borderRadius: 10,
+        padding: 10,
+        height: '73%',
+        width : '90%',
+        marginBottom : '4%',
+        marginTop :'2%'
+       }} onPress={() => navigation.navigate("Product")}>
+        <View style={{padding : 10}}>
+            <View style = {{
+                    marginTop : '1%',
+                    justifyContent : 'space-between',
+                    alignItems : 'center',
+                }}>
+                <View style={{flexDirection : 'row', alignItems : 'center',  marginLeft : '2%'}}>
+                    <TouchableOpacity>
+                        <Image source={require('./assets/back.png')}/>
+                    </TouchableOpacity>
+                    <Text style={{
+                        fontSize: 18,
+                        fontWeight: 'bold',
+                        color : '#1D3557',
+                        marginLeft : '5%'
+                        }}>
+                        {name}
+                    </Text>
+                    <Text style={{fontSize : 20, marginLeft : '50%'}}> {accquired} / {total} </Text>
+                </View>
+            </View>
+            <Image source={image} style={{height : '60%', width : '100%', justifyContent:'center', marginTop : '2%'}} resizeMode="contain" />
+            <View style={{alignItems : 'center', flexDirection : 'row', flex : 1, justifyContent : 'center'}}>
+                <View style={{backgroundColor : '#ADADAD', height : 10, width : 10, borderRadius : 5, marginHorizontal : '5%'}}></View>
+                <View style={{backgroundColor : '#D9D9D9', height : 10, width : 10, borderRadius : 5, marginHorizontal : '5%'}}></View>
+            </View>
+            <View style={{ alignItems: 'center' }}>
+        </View>
+            <Text style={{fontSize: 18, color:'#1D3557', marginTop : '4%',fontFamily:'KiwiMaru_400Regular'}}>{description}</Text>
+            <View style={{
+                marginTop : '5%',
+                flexDirection : 'row',
+                alignItems : 'center'
+                }}>
+                <Text style={{
+                    fontSize: 16,
+                    color: 'black', 
+                    textAlign : 'left',
+                    fontFamily:'KiwiMaru_400Regular'
+                    }}>PRICE: {price}
+                </Text>
+                <Image source={require('./assets/coins.png')} style={{height : 22, width : 22, marginLeft : '2%'}}></Image>
+            </View>
+            <View style={{
+                flexDirection : 'row',
+                marginTop : '8%',
+                justifyContent : 'center'
+            }}>
+                <TouchableOpacity style={{ height : 60, width : '50%', backgroundColor: 'rgba(255, 238, 221, 0.8)', justifyContent : 'center', borderRadius: 10, borderColor : '#FFDCB9', borderBottomWidth: 3}}>
+                    <Text style={{fontSize : 18, textAlign : 'center',fontFamily:'KiwiMaru_400Regular'}}>BUY NOW</Text>
+                </TouchableOpacity>
+                <TouchableOpacity style={{ height : 60, width : '50%', backgroundColor: 'rgba(255, 238, 221, 0.8)', marginLeft : '4%', justifyContent :'center', borderRadius: 10, borderColor : '#FFDCB9', borderBottomWidth: 3}}>
+                    <Text style={{fontSize : 18, textAlign : 'center',fontFamily:'KiwiMaru_400Regular'}}>WISHLIST</Text>
+                </TouchableOpacity>
+            </View>
+        </View>
+      </View>
+    );
+};
+
+export default function Product() {
+  return (
+    <SafeAreaView style={styles.container}>
+    <View style={{
+        height: 50,
+        flexDirection: "row",
+        verticalAlign:'top',
+        marginTop : '13%',
+        paddingRight: 20,
+        paddingLeft: 20
+    }}>
+        <Text style={{
+            fontSize: 25,
+            color : '#232ED1',
+            fontWeight : 'bold',
+            textAlign : 'left',
+        }}>BHARAT VIDHI</Text>
+
+        <View style={{
+            flex: 1,
+            justifyContent: 'space-around',
+            flexDirection: "row",
+            alignSelf: "stretch",
+            marginLeft : '16%'
+        }}>
+            <Image source={require('./assets/notification.png')} styles={{marginRight : '3%'}}></Image>
+            <Image source={require('./assets/coins.png')}></Image>
+            <Image source={require('./assets/profile.png')}></Image>
+        </View>
+    </View>
+    <Text style={styles.archives1}>ARCHIVES</Text>
+        <ProductDisplay 
+            bgColor={'#FFEEDD'} 
+            name={'NAME'} 
+            description={'DESCRIPTION -- --'} 
+            price={'15'} 
+            image={source=require('./assets/stamp.png')} 
+            total={5}
+            accquired={0}/>
+    <Footer></Footer>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+    archives1 : {
+        fontSize: 28,
+        color: "#080606",
+        textAlign: "center",
+        marginTop:'2%',
+        fontFamily:'PatrickHandSC_400Regular'
+    },
+    container: {
+        backgroundColor: 'white',
+        alignItems:'center',
+        width:'100%',
+        height:'100%',
+    },
+    scroll:{
+        width : 340,
+        borderRadius : 10,
+        marginTop : '5%',
+        marginBottom : '10%'
+    }
+    });
---
+++ Profile.js
@@ -0,0 +1,283 @@
+
+import { StyleSheet, Text, View, Image, Platform, StatusBar, TouchableOpacity, ScrollView, ImageBackground, SafeAreaView, Button} from 'react-native';
+import Footer from './Footer';
+import { useFonts, PatrickHandSC_400Regular } from '@expo-google-fonts/patrick-hand-sc';
+import React, { useState } from 'react';
+import { Itim_400Regular } from '@expo-google-fonts/itim';
+import { Iceland_400Regular } from '@expo-google-fonts/iceland';
+const Streaks=0;
+const level=0;
+const badge=0;
+
+const Details = ({ name, age,email }) => {
+    return (
+        <View >
+            <Text style={styles.info}>{name}</Text>
+            <Text style={styles.info}>{age}</Text>
+            <Text style={styles.info}>{email}</Text>
+        </View>
+
+        
+    );
+};
+
+export default function Profile() {
+    const [fontsLoaded] = useFonts({
+        PatrickHandSC_400Regular,
+        Itim_400Regular,
+        Iceland_400Regular
+      });
+    return (
+        <SafeAreaView style={{
+            flex: 1
+        }}>
+            <View style={{
+                marginTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+                paddingRight: 20,
+                paddingLeft: 20
+            }}>
+                <View style={{
+                    height: 60,
+                    paddingTop: 10,
+                    paddingBottom: 10,
+                    flexDirection: "row"
+                }}>
+                    <Text style={{
+                        fontSize: 25,
+                        flex: 2,
+                        color : '#232ED1',
+                        fontWeight : 'bold'
+                    }}>BHARAT VIDHI</Text>
+
+                    <View style={{
+                        flex: 1,
+                        justifyContent: 'space-around',
+                        flexDirection: "row",
+                        alignSelf: "stretch",
+                        marginRight : '2%'
+                    }}>
+                        <Image source={require('./assets/notification.png')} />
+                        <Image source={require('./assets/coins.png')} />
+                        <Image source={require('./assets/profile.png')} />
+                    </View>
+                </View>
+            </View>
+            <ScrollView >
+                <View style={styles.bg}>
+                    <ImageBackground source={require('./assets/profile_bg.png')} style={styles.bg}>
+                <Image source={require('./assets/Profile_page.png')} style={styles.pfp}></Image>
+                        <View style={styles.data}> 
+                            <Details name={"Darshan"} age={'17'} email={'d..@gmail.com'}/>
+                            <View style={{flexDirection:'row',gap:20}}>
+                                <View style={styles.block}>
+                                 <Text style={{fontSize:18,alignSelf:'center',top:20,fontFamily : 'Itim_400Regular'}}>{Streaks}</Text>
+                                    <Image source={require('./assets/Fire.png')} style={{alignSelf:'center',top:'30%'}} />
+                                </View>
+                                <View style={styles.block}>
+                                <Text style={{fontSize:18,alignSelf:'center',top:20,fontFamily : 'Itim_400Regular'}}>{level}</Text>
+                                <Image source={require('./assets/Scoring.png')} style={{alignSelf:'center',top:'30%'}} />
+                                </View>
+                                <View style={styles.block}>
+                                <Text style={{fontSize:18,alignSelf:'center',top:20,fontFamily : 'Itim_400Regular'}}>{badge}</Text>
+                                <Image source={require('./assets/Premium Badge.png')} style={{alignSelf:'center',top:'30%'}} />
+                                </View>
+                            </View>
+                            <View style={{flexDirection:'row',gap:85,top:105,paddingLeft:28}}>
+                                <Text style={{fontSize:12,color:'#A4AED3',fontFamily : 'Itim_400Regular'}}>Streaks</Text>
+                                <Text style={{fontSize:12,color:'#A4AED3',fontFamily : 'Itim_400Regular'}}>Level</Text>
+                                <Text style={{fontSize:12,color:'#A4AED3',fontFamily : 'Itim_400Regular'}}>Badges</Text>
+                            </View>
+                            <View style={styles.score}>
+                                <Text style={{fontSize:24,left:20,fontFamily:'Iceland_400Regular'}}>Statistics</Text>
+                                
+                                <View style={{flexDirection:'row',gap:20,paddingLeft:30}} > 
+                                    <Image source={require('./assets/Pie chart.png')} style={{height:120,width:120}}/>
+                                   <Text style={{fontFamily:'Iceland_400Regular',alignSelf:'center'}}>User stats</Text> 
+                                </View>
+                                <View style={{flexDirection:'column',gap:10}}>
+                                <Text style={{fontFamily:'Iceland_400Regular',alignSelf:'center'}}>Leader board stats</Text>
+                                <View style={{flexDirection:'row',gap:20,paddingLeft:10,alignSelf:'center'}}>
+                                <View style={{height:50,width:50,backgroundColor:'#f7d108',borderRadius:5}}>
+                                    <Text style={{fontFamily:'Iceland_400Regular',fontSize:12,top:10,alignSelf:'center'}}>0</Text>
+                                    <Text style={{fontFamily:'Iceland_400Regular',fontSize:12,top:20,alignSelf:'center'}}>Zonal</Text>
+                                </View>
+                                <View style={{height:50,width:50,backgroundColor:'#f7d108',borderRadius:5}}>
+                                <Text style={{fontFamily:'Iceland_400Regular',fontSize:12,top:10,alignSelf:'center'}}>0</Text>
+                                <Text style={{fontFamily:'Iceland_400Regular',fontSize:12,top:20,alignSelf:'center'}}>State</Text>
+                                </View>
+                                <View style={{height:50,width:50,backgroundColor:'#f7d108',borderRadius:5}}>
+                                <Text style={{fontFamily:'Iceland_400Regular',fontSize:12,top:10,alignSelf:'center'}}>0</Text>
+                                <Text style={{fontFamily:'Iceland_400Regular',fontSize:12,top:20,alignSelf:'center'}}>National</Text>
+                                </View>
+                                </View>
+                                </View>
+                                <View style={{flexDirection:'row',gap:20,paddingLeft:30}}>
+                                <Text style={{fontFamily:'Iceland_400Regular',alignSelf:'center'}}>Civic mastery stats</Text> 
+                                <Image source={require('./assets/Analysis.png')} style={{height:130,width:130}}/>
+                                </View>
+                            </View>
+                            <View style={styles.set1}>
+                                <Text style={{fontSize:18,fontFamily : 'Itim_400Regular'}}>Personal Information</Text>
+                                <TouchableOpacity>
+                                <View style={{flexDirection:'row',gap:20,paddingLeft:20}} onPress={() => navigation.navigate('Learning')}>
+                                    <Image source={require('./assets/Card game.png')} style={{height:30,width:30}}/>
+                                    <Text style={{fontSize:18,fontFamily : 'Itim_400Regular'}}>Card Deck</Text>
+                                </View>
+                                </TouchableOpacity>
+                               <TouchableOpacity>
+                               <View style={{flexDirection:'row',gap:20,paddingLeft:20}} onPress={() => navigation.navigate('Learning')}>
+                                    <Image source={require('./assets/Stamp.png')} style={{height:30,width:30}} />
+                                    <Text style={{fontSize:18,fontFamily : 'Itim_400Regular'}}>My Collection</Text>
+                                </View>
+                               </TouchableOpacity>
+                                <TouchableOpacity>
+                                <View style={{flexDirection:'row',gap:20,paddingLeft:20}} onPress={() => navigation.navigate('Learning')}>
+                                    <Image source={require('./assets/Collaboration.png')} style={{height:30,width:30}}/>
+                                    <Text style={{fontSize:18,fontFamily : 'Itim_400Regular'}}>Team</Text>
+                                </View>
+                                </TouchableOpacity>
+                                
+                            </View>
+                            <View style={styles.set2}>
+                                <Text style={{fontSize:18 ,fontFamily : 'Itim_400Regular'}}>App Settings </Text>
+                                <TouchableOpacity>
+                                <View style={{flexDirection:'row',gap:20,paddingLeft:20}} onPress={() => navigation.navigate('Learning')}>
+                                    <Image source={require('./assets/Headphones.png')} style={{height:30,width:30}}/>
+                                    <Text style={{fontSize:18,fontFamily : 'Itim_400Regular'}}>Help & Support</Text>
+                                </View>
+                                </TouchableOpacity>
+                               <TouchableOpacity>
+                               <View style={{flexDirection:'row',gap:20,paddingLeft:20}} onPress={() => navigation.navigate('Learning')}>
+                                    <Image source={require('./assets/Help.png')} style={{height:30,width:30}} />
+                                    <Text style={{fontSize:18,fontFamily : 'Itim_400Regular'}}>FAQs</Text>
+                                </View>
+                               </TouchableOpacity>
+                                <TouchableOpacity>
+                                <View style={{flexDirection:'row',gap:20,paddingLeft:20}} onPress={() => navigation.navigate('Learning')}>
+                                    <Image source={require('./assets/Cloud computing.png')} style={{height:30,width:30}}/>
+                                    <Text style={{fontSize:18,fontFamily : 'Itim_400Regular'}}>Privacy Policy</Text>
+                                </View>
+                                </TouchableOpacity>
+                                <TouchableOpacity>
+                                <View style={{flexDirection:'row',gap:20,paddingLeft:20}} onPress={() => navigation.navigate('Learning')}>
+                                    <Image source={require('./assets/Key.png')} style={{height:30,width:30}}/>
+                                    <Text style={{fontSize:18,fontFamily : 'Itim_400Regular'}}>Change Password</Text>
+                                </View>
+                                </TouchableOpacity>
+                                <TouchableOpacity>
+                                <View style={{flexDirection:'row',gap:20,paddingLeft:20}} onPress={() => navigation.navigate('Learning')}>
+                                    <Image source={require('./assets/Star.png')} style={{height:30,width:30}}/>
+                                    <Text style={{fontSize:18,fontFamily : 'Itim_400Regular'}}>Rate us</Text>
+                                </View>
+                                </TouchableOpacity>
+                                <TouchableOpacity>
+                                <View style={{flexDirection:'row',gap:20,paddingLeft:20}} onPress={() => navigation.navigate('Learning')}>
+                                    <Image source={require('./assets/Settings.png')} style={{height:30,width:30}}/>
+                                    <Text style={{fontSize:18,fontFamily : 'Itim_400Regular'}}>Preferences</Text>
+                                </View>
+                                </TouchableOpacity>
+                                <TouchableOpacity>
+                                <View style={{flexDirection:'row',gap:20,paddingLeft:20}} onPress={() => navigation.navigate('Learning')}>
+                                    <Image source={require('./assets/Add friend.png')} style={{height:30,width:30}}/>
+                                    <Text style={{fontSize:18,fontFamily : 'Itim_400Regular'}}>Invite a friend</Text>
+                                </View>
+                                </TouchableOpacity>
+                                <TouchableOpacity>
+                                <View style={{flexDirection:'row',gap:10,alignSelf:'center',top:30}} onPress={() => navigation.navigate('Learning')}>
+                                    <Image source={require('./assets/copyright.png')} style={{height:12,width:12}}/>
+                                    <Text style={{fontsize:12,textDecorationLine:'underline' ,color:'#A4AED3',fontFamily : 'Itim_400Regular'}}>Terms and Conditions</Text>
+                                
+                                </View>
+                                   
+                                </TouchableOpacity>
+                
+                            </View>
+                        </View>
+                        <TouchableOpacity>
+                        <View style={styles.logout}>
+                        <Text style={{fontSize:18,alignSelf:'center',top:10,fontFamily : 'Itim_400Regular'}}>Log out</Text>
+                       </View>
+                        </TouchableOpacity>
+                       
+                    </ImageBackground>
+                </View>
+                
+                
+            </ScrollView>
+            <Footer></Footer>
+        </SafeAreaView>
+    );
+}
+
+const styles = StyleSheet.create({
+
+    container: {
+        backgroundColor: 'white',
+        
+    },
+    logout:{
+        alignSelf:'center',
+        backgroundColor:'white',
+        top:120,
+        height:40,
+        width:140,
+        borderRadius:10,
+        opacity:0.7
+    },
+    set1:{
+        top:140,
+        gap:20
+    },
+    set2:{
+        top:170,
+        gap:20 
+    },
+    score:{
+       
+        top:120,
+        height:500,
+        width:350,
+        gap:50,
+        backgroundColor:'#4C66F8',
+        borderRadius:20,
+        fontFamily:'Iceland_400Regular'
+    },
+    block:{
+       
+        top:100,
+        height:100,
+        width:100,
+        backgroundColor:'#FFEEDD',
+        
+        borderRadius:10
+    },
+    info:{
+        fontSize:24,
+        textAlign:'center',
+        top:60,
+        fontFamily : 'Itim_400Regular'
+    },
+    pfp:{
+        top:150,
+        alignSelf:'center',
+        zIndex:2
+    },
+    bg:{
+       
+        borderRadius:15,
+        height:2000,
+        
+        width:'100%',
+    },
+    data:{
+        backgroundColor:'white',
+        top:'4%',
+        borderRadius:70,
+        height:1555,
+        width:'100%',
+        zIndex:1,
+        padding:20,
+        
+        
+    },
+});
